6da878e28a77645f9b2e0f606f16912f224751c4
Leo Lamprecht <mindrun@icloud.com>
2016-06-26 16:10:12 +0200

    Added basic logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,58 @@
 import request from 'request-promise'
 
-const options = {
-  uri: 'http://uinames.com/api/',
-  qs: {
-    amount: 25
-  },
+let options = {
+  uri: 'http://uinames.com/api',
   json: true
 }
 
-request(options).then(function(repos) {
-  console.log(repos)
-})
+const allowedParams = {
+  amount: Number,
+  gender: [
+    'male',
+    'female'
+  ],
+  region: String,
+  minLength: Number,
+  maxLength: Number
+}
+
+const wrongType = (param, type) => `Value for parameter "${param}" needs to be of type "${type}"`
+
+const isAllowed = params => {
+  if (!params) return false
+
+  for (let param in params) {
+    let inList = allowedParams[param],
+        content = params[param]
+
+    if (!inList) throw Error(`Parameter "${param}" is not allowed`)
+
+    if (Array.isArray(inList)) {
+      if (typeof content != 'string') {
+        throw Error(wrongType(param, 'String'))
+      }
+
+      if (!inList.includes(content)) {
+        throw Error(`"${content}" is not allowed as a value for "${param}"`)
+      }
+    } else if (content.constructor != inList){
+      throw Error(wrongType(param, inList.name))
+    }
+  }
+}
+
+export default (config) => {
+  let allowed
+
+  try {
+    allowed = isAllowed(config)
+  } catch (err) {
+    return console.error(err)
+  }
+
+  if (allowed) {
+    options.qs = config
+  }
+
+  return request(options)
+}
---
+++ test/index.js
@@ -0,0 +1,21 @@
+import test from 'ava'
+import names from '../dist'
+
+const isObject = obj => typeof obj === 'object' && obj !== null
+
+test.beforeEach('requesting data', async t => {
+  let list
+
+  try {
+    list = await names()
+  } catch (err) {
+    t.fail(err)
+  }
+
+  t.context = list
+  t.pass()
+})
+
+test('response is valid object', async t => {
+  console.log(t.context)
+})
